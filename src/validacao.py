6946d2fc392dc9053a1ee1175517634042790eb4
Tulio Dantas <129971892+DevTulioDantas@users.noreply.github.com>
2026-08-15 15:00:06 -0400

    feat: atualiza apenas o arquivo validacao.py

--- src/validacao.py
+++ src/validacao.py
@@ -1,4 +1,6 @@
 import re
+from datetime import datetime
+
 
 def email_valido(email:str) -> bool:
     """_summary_
@@ -20,6 +22,34 @@ def email_valido(email:str) -> bool:
     return bool (padrao_email.fullmatch(email))
 
 
+def padronizar_data(data_bruta:str) -> str:
+    """Verifica o formato que a data foi lançada e retorna no padrão DD/MM/AAAA
+
+    Args:
+        data_bruta (str): Data sem o tratamento
+
+    Returns:
+        str: Data no formato DD/MM/AAAA ou none se a data for invalida ou não corresponder a nenhum formato conhecido previsto
+    """
+    formatos_data_aceito = [
+        "%d/%m/%Y",   # 
+        "%Y-%m-%d",   
+        "%d-%m-%Y",   
+    ]
+
+    if not data_bruta:
+        return None
+
+    for formato in formatos_data_aceito :
+        try:
+            data_convertida = datetime.strptime(data_bruta.strip(), formato)
+            return data_convertida.strftime("%d/%m/%Y") #String format time
+        except ValueError:
+            continue
+
+    return None
+
+      
 
 
 
