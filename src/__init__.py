20915340195bb0d12452b929fd7d1c6bc4a68e5f
Leandro Souza <leandro.souza3322@gmail.com>
2026-08-16 11:33:21 -0300

    Configuração do __init__ das funções 1.0.0

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,34 @@
 
+#leitura.py
+from .leitura import (
+    carregar_csv,
+    carregar_excel,
+    carregar_json,
+    carregar_txt,
+    verificar_e_criar_diretorios,
+)
+
+from .validacao import (
+    email_valido,
+    padronizar_data,
+    padronizar_telefone,
+)
+
+
+# Para importar como " from src import * "
+__all__ = [
+    # Funções de leitura
+    "carregar_csv",
+    "carregar_excel",
+    "carregar_json",
+    "carregar_txt",
+    "verificar_e_criar_diretorios",
+    
+    #Funções de validação
+    "email_valido",
+    "padronizar_data",
+    "padronizar_telefone",
+
+]
+
+__version__= "1.0.0"
